c03559b923a6d60a277f6f9f3986f97b47e83fec
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2017-09-19 09:39:23 +0200

    Fix split of options

--- support/src/main/java/nl/futureedge/maven/docker/executor/Docker.java
+++ support/src/main/java/nl/futureedge/maven/docker/executor/Docker.java
@@ -6,6 +6,13 @@ import java.util.List;
 
 public final class Docker {
 
+    private static final String REGISTRY_SEPARATOR = "/";
+    private static final String TAG_SEPARATOR = ":";
+
+    private static final String ALL_WHITESPACE = "\\s+";
+    private static final String SINGLE_SPACE = " ";
+
+
     public static String getImage(final String registry, final String name, final String tag) {
         if (name == null || "".equals(name.trim())) {
             return name;
@@ -14,15 +21,15 @@ public final class Docker {
         final StringBuilder image = new StringBuilder();
         if (registry != null && !"".equals(registry.trim())) {
             image.append(registry);
-            if (!registry.endsWith("/")) {
-                image.append('/');
+            if (!registry.endsWith(REGISTRY_SEPARATOR)) {
+                image.append(REGISTRY_SEPARATOR);
             }
         }
 
         image.append(name);
 
         if (tag != null && !"".equals(tag.trim())) {
-            image.append(':');
+            image.append(TAG_SEPARATOR);
             image.append(tag);
         }
 
@@ -30,8 +37,9 @@ public final class Docker {
     }
 
     public static List<String> splitOptions(final String options) {
-        if ((options != null) && !"".equals(options)) {
-            return Arrays.asList(options.trim().split(" "));
+        if ((options != null) && !"".equals(options.trim())) {
+            final String normalized = options.replaceAll(ALL_WHITESPACE, SINGLE_SPACE).trim();
+            return Arrays.asList(normalized.split(SINGLE_SPACE));
         } else {
             return Collections.emptyList();
         }
