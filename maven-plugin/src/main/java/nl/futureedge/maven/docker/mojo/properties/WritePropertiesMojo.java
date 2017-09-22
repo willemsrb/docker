1b240c839c90d68d87b6a2f5248389c4f79eb0b4
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2017-09-22 12:06:43 +0200

    Writing of property files can be merged with existing files

--- maven-plugin/src/main/java/nl/futureedge/maven/docker/mojo/properties/WritePropertiesMojo.java
+++ maven-plugin/src/main/java/nl/futureedge/maven/docker/mojo/properties/WritePropertiesMojo.java
@@ -1,9 +1,11 @@
 package nl.futureedge.maven.docker.mojo.properties;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Properties;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -11,6 +13,9 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "write-properties", requiresProject = false)
 public final class WritePropertiesMojo extends AbstractPropertiesMojo {
+
+    @Parameter(name = "mergePropertyFiles", property = "properties.merge")
+    private File[] mergePropertyFiles;
 
     /**
      * Target property file (will be overwritten if it exists).
@@ -20,9 +25,23 @@ public final class WritePropertiesMojo extends AbstractPropertiesMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        // Filtered properties
+        final Properties result = getProperties();
+
+        // Merge
+        if (mergePropertyFiles != null) {
+            for (final File mergePropertyFile : mergePropertyFiles) {
+                try (final FileInputStream is = new FileInputStream(mergePropertyFile)) {
+                    result.load(is);
+                } catch (IOException e) {
+                    throw new MojoFailureException("Could not load property file to merge", e);
+                }
+            }
+        }
+
         // Write
         try (final OutputStream out = new FileOutputStream(target)) {
-            getProperties().store(out, null);
+            result.store(out, null);
         } catch (IOException e) {
             throw new MojoFailureException("Could not write property file", e);
         }
